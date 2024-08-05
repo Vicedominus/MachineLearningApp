f7523f65d14c8756cce051424fdcf0ae272b0365
avice <avicedor@gmail.com>
2024-08-05 05:41:23 -0300

    streamlit app data show

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,20 @@
 import streamlit as st
+import pandas as pd
 
 st.title('🎈 App Name')
 
 st.write('Hello world!')
+st.info("Info about the app")
+
+with st.expander('Data'):
+  st.write('**Raw data**')
+  df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
+  df
+
+  st.write('**X**')
+  X_raw = df.drop('species', axis=1)
+  X_raw
+
+  st.write('**y**')
+  y_raw = df.species
+  y_raw
